9d19aa192063f94d22c722e47060f0c9c38deb53
emmanuelbayu <emmanuelbayu26@gmail.com>
2021-10-13 11:23:54 +0700

    edit dbConnection

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 require('dotenv/config')
 
-mongoose
+const dbConnect = () => {
+return mongoose
 .connect(process.env.MONGO_URI,{useNewUrlParser : true, useUnifiedTopology : true})
 .then((res) => console.log("Connected to database successfully"))
 .catch((err) => console.log(err));
+};
+
+module.exports = dbConnect;
